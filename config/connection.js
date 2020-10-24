6c10c8e9f7af29ffd1d86dbe0264ea559c7fbba4
Matthew O <mo@pop-os.localdomain>
2020-10-23 18:50:27 -0700

    fixed app to work in herokuk

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,19 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // establishes connection to db
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+let sequelize;
 
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
